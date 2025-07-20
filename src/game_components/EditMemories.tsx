bb41c541e2d3c9cc0896da59c6c272432a34280f
Joe Farah <joseph.farah100@gmail.com>
2025-07-20 00:14:06 -0400

    Update toolbar for Edit memories

--- src/game_components/EditMemories.tsx
+++ src/game_components/EditMemories.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {useState} from 'react'
-import { useNavigate, useParams, useLocation} from "react-router-dom";
+import {motion} from "motion/react"
+import { useNavigate, useParams, useLocation, Link} from "react-router-dom";
 import '../assets/styles/add-game.css';
 import { fetchWithCSRF } from "../assets/scripts/csrf";
 import Editor from "..//general/Editor.tsx"
@@ -72,9 +73,25 @@ function EditMemories() {
    
   return (
 
-    <div className= "">
-       <h1 className="page-header">Edit Memories of {title} </h1> 
+    <div className= "page-header">
+       <div className= "flex items-center gap-3">
+       <h1 className="flex items-center">Edit Memories of {title} </h1> 
+       <motion.div 
 
+       whileHover = {{
+        scale:1.1,
+        x:2,
+       }}
+       >
+
+      <Link to={{pathname: `/dashboard/games/${id}/${title}`}}> 
+
+      <svg className= "fill-[var(--accent-clr)]"  xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M360-240 120-480l240-240 56 56-144 144h488v-160h80v240H272l144 144-56 56Z"/></svg>
+      
+       </Link>
+       </motion.div>
+
+      </div>
       <form onSubmit= {submitMemories}>
       <Editor memories = {edit_memories} handleSubmit ={submitMemories} handleChange={setNewMemories}/>
        </form>
